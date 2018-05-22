a964f6445f2589d64d1ca091781953f4a57f49c2
ivan <ivan@datasalt.com>
2018-05-22 09:16:56 +0200

    xception-bs-32-lr0.5-mom0.5-wd1e-5-cutout4-minscale0.4-rota15

--- run_infer.py
+++ run_infer.py
@@ -7,6 +7,7 @@ import torch
 from torchvision import models
 import matplotlib.pyplot as plt
 
+import sexception
 from data import get_data_loader
 from measures import f1_score, reduce_stats, multilabel_stats
 from models import get_resnet_model, get_model
@@ -62,7 +63,7 @@ resource.setrlimit(resource.RLIMIT_NOFILE, (40000, rlimit[1]))
 rlimit = resource.getrlimit(resource.RLIMIT_NOFILE)
 print("LIMIT after: {}".format(rlimit))
 
-img_set_folder = "data/validation"
+img_set_folder = "data/test"
 model_file= "runs/"+ "May11_05-47-56_cs231n-1resnet101-bs-64-clr5e-6-0.05-mom0.9-imgsize-224" + "/model_best.pth.tar" # 0.502
 model_file= "runs/"+ "May11_09-34-42_cs231n-1resnet101-bs-64-clr1e-5-0.1-mom0.9-imgsize-224-pos-weight3" + "/model_best.pth.tar" # 0.599
 model_file = "runs/"+ "May16_13-38-21_cs231n-1resnet101-bs-64-lr0.01-mom0.9-wd4e-4-pos-weight3" + "/model_best.pth.tar" # 0.603
@@ -71,8 +72,12 @@ model_file = "runs/"+ "May17_16-04-29_cs231n-1xception-bs-64-lr0.045-mom0.9-wd1e
 model_file = "runs/"+ "May18_07-37-59_cs231n-1xception-bs-64-lr0.045-mom0.9-wd1e-5-pos-weight3-just-fc" + "/model_best.pth.tar" # 0.45
 model_file = "runs/"+ "May20_08-35-03_cs231n-1xception-bs-32-clr0.01-0.001-mom0.9-wd1e-5-pos-weight3" + "/model_best.pth.tar" # 0.6036
 model_file = "runs/"+ "May20_22-27-03_cs231n-1xception-bs-32-clr0.01-0.001-mom0.9-wd1e-5-pos-weight3-cutout4-minscale0.4" + "/model_best.pth.tar" # 0.6038
+model_file = "runs/"+ "May21_17-04-17_cs231n-1sexception-bs-38-lr0.1-mom0.9-wd1e-5-pos-weight3-cutout4-minscale0.4-rota15" + "/model_best.pth.tar" # 0.6541
+model_file = "runs/"+ "May21_17-04-17_cs231n-1sexception-bs-38-lr0.1-mom0.9-wd1e-5-pos-weight3-cutout4-minscale0.4-rota15" + "/model_best.pth.tar" # 0.6541
+#model_file = "runs/"+ "May21_22-09-11_cs231n-1sexception-bs-38-clr0.001-0.01-mom0.9-wd1e-5-pos-weight3-cutout4-minscale0.4-rota15" + "/model_best.pth.tar" # 0.6539
 
-model_type="xception"
-model_class=pretrainedmodels.xception
 
-run(img_set_folder, model_file, model_class, model_type, 'validation', samples_limit=1000, tta=False, batch_size=64)
+model_type="sexception"
+model_class=sexception
+
+run(img_set_folder, model_file, model_class, model_type, 'test', samples_limit=None, tta=False, batch_size=64)
--- run_train.py
+++ run_train.py
@@ -44,15 +44,16 @@ model_file = "runs/"+ "May18_08-53-22_cs231n-1xception-bs-64-lr0.045-mom0.9-wd1e
 model_file = "runs/"+ "May18_20-13-42_cs231n-1xception-bs-64-lr0.0045-mom0.9-wd1e-5-pos-weight3-from-block3" + "/model_best.pth.tar" # 0.5688
 model_file = "runs/"+ "May19_05-43-08_cs231n-1xception-bs-32-lr0.1-mom0.9-wd1e-5-pos-weight3" + "/model_best.pth.tar" # 0.5991
 model_file = "runs/"+ "May20_08-35-03_cs231n-1xception-bs-32-clr0.01-0.001-mom0.9-wd1e-5-pos-weight3" + "/model_best.pth.tar" # 0.6036
-model_file = "runs/"+ "May20_22-27-03_cs231n-1xception-bs-32-clr0.01-0.001-mom0.9-wd1e-5-pos-weight3-cutout4-minscale0.4" + "/model_best.pth.tar" # 0.6038
-model_file = "runs/"+ "May21_14-13-52_cs231n-1sexception-bs-32-clr0.1-0.01-mom0.9-wd1e-5-pos-weight3-cutout4-minscale0.4-just-se-modules" + "/model_best.pth.tar" # 0.6452
-model_file = "runs/"+ "May21_15-14-40_cs231n-1sexception-bs-38-lr0.1-mom0.9-wd1e-5-pos-weight3-cutout4-minscale0.4" + "/model_best.pth.tar" # 0.6496
-model_file = "runs/"+ "May21_17-04-17_cs231n-1sexception-bs-38-lr0.1-mom0.9-wd1e-5-pos-weight3-cutout4-minscale0.4-rota15" + "/model_best.pth.tar" # 0.6541
+model_file = "runs/"+ "May20_22-27-03_cs231n-1xception-bs-32-clr0.01-0.001-mom0.9-wd1e-5-pos-weight3-cutout4-minscale0.4" + "/model_best.pth.tar" # 0.6038 "Best by now"
+#model_file = "runs/"+ "May21_14-13-52_cs231n-1sexception-bs-32-clr0.1-0.01-mom0.9-wd1e-5-pos-weight3-cutout4-minscale0.4-just-se-modules" + "/model_best.pth.tar" # 0.6452
+#model_file = "runs/"+ "May21_15-14-40_cs231n-1sexception-bs-38-lr0.1-mom0.9-wd1e-5-pos-weight3-cutout4-minscale0.4" + "/model_best.pth.tar" # 0.6496
+#model_file = "runs/"+ "May21_17-04-17_cs231n-1sexception-bs-38-lr0.1-mom0.9-wd1e-5-pos-weight3-cutout4-minscale0.4-rota15" + "/model_best.pth.tar" # 0.6541
+#model_file = "runs/"+ "May21_22-09-11_cs231n-1sexception-bs-38-clr0.001-0.01-mom0.9-wd1e-5-pos-weight3-cutout4-minscale0.4-rota15" + "/model_best.pth.tar" # 0.6539
 
 #model_type = "resnet101"
 #model_type = "nasnetlarge"
-#model_type = "xception"
-model_type = "sexception"
+model_type = "xception"
+#model_type = "sexception"
 
 pretrained=False
 if not model_file:
@@ -66,7 +67,7 @@ data_dir = 'data'
 image_datasets, dataloaders = {}, {}
 for set in sets:
   folder = os.path.join(data_dir, set)
-  id, dl = get_data_loader(folder, model_type, set, annotations, batch_size=38)
+  id, dl = get_data_loader(folder, model_type, set, annotations, batch_size=32)
   image_datasets[set] = id
   dataloaders[set] = dl
 
@@ -77,8 +78,8 @@ num_classes = len(class_names)
 
 # see https://www.tensorflow.org/api_docs/python/tf/nn/weighted_cross_entropy_with_logits_with_logits. Balance between recall/precision
 #pos_weight=torch.from_numpy(class_freq/(1-class_freq)).type(torch.float).to(device)
-pos_weight=torch.from_numpy(np.ones(num_classes) * 3).type(torch.float).to(device)
-#pos_weight=None
+#pos_weight=torch.from_numpy(np.ones(num_classes) * 3).type(torch.float).to(device)
+pos_weight=None
 # Using BCEWithLogitsLoss because it seems to have better numerical stability than
 # using MultiLabelSoftMarginLoss or combining a sigmoid con BCELoss
 criterion = pytorch_patches.BCEWithLogitsLoss(pos_weight=pos_weight, label_smoothing=0)
@@ -91,15 +92,15 @@ criterion = pytorch_patches.BCEWithLogitsLoss(pos_weight=pos_weight, label_smoot
 # https://github.com/tensorflow/models/blob/master/research/slim/nets/nasnet/nasnet.py
 #optimizer_ft = optim.RMSprop(list(model.last_linear.parameters()) + list(model.cell_17.parameters()), lr=0.1, weight_decay=0.00004, alpha=0.9, eps=1, momentum=0.9)
 #optimizer_ft = optim.SGD(model.parameters(), lr=0.0001, momentum=0.9, weight_decay=0.0001) # resnet
-optimizer_ft = optim.SGD(model.parameters_to_train, lr=0.01, momentum=0.9, weight_decay=1e-5) # xception
+optimizer_ft = optim.SGD(model.parameters_to_train, lr=0.5, momentum=0.5, weight_decay=1e-5) # xception
 
 #exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=20, gamma=0.1)
 #exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=1, gamma=2) # for lr testing
 lr_f = lambda x: sawtooth(0.0001, 1, 3, x)
 lr_f = lambda x: sawtooth(0.1, 1, 2, x)
-exp_lr_scheduler = lr_scheduler.LambdaLR(optimizer_ft, lr_f)#lambda x: 1)
+exp_lr_scheduler = lr_scheduler.LambdaLR(optimizer_ft, lambda x: 1)
 
-trainer = Trainer("sexception-bs-38-clr0.001-0.01-mom0.9-wd1e-5-pos-weight3-cutout4-minscale0.4-rota15",
+trainer = Trainer("xception-bs-32-lr0.5-mom0.5-wd1e-5-cutout4-minscale0.4-rota15",
                   model,
                   criterion,
                   optimizer_ft,
